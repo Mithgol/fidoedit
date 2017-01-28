23d4a5b35ec180559d9ef4891bb614ce943e5c35
Mithgol the Webmaster <getgit@mithgol.ru>
2017-01-28 22:09:02 +0300

    add Twitter Emoji from MaxCDN (with SRI hash), separate core script

---
+++ fe-core.js
@@ -0,0 +1,30 @@
+﻿/*global $, emojify: true, nw, twemoji */
+
+/* var; TODO: use it */ emojify = () => {
+   if( twemoji ) twemoji.parse( $('body')[0] );
+};
+
+var errorCLI = errHTML => $('.purposeMain').html([
+   '<div class="alert alert-danger" role="alert">',
+   errHTML, '</div>'
+].join(''));
+
+$(() => {
+   var paramsCLI = nw.App.argv;
+
+   var msgFilePath = paramsCLI.find( param => param.startsWith('--file=') );
+   if( typeof msgFilePath === 'undefined') return errorCLI(
+      'Filename has not been given.'
+   );
+   msgFilePath = msgFilePath.slice( '--file='.length );
+
+   var lineNum = paramsCLI.find( param => param.startsWith('--line=') );
+   if( typeof lineNum === 'undefined') return errorCLI(
+      'Line number has not been given.'
+   );
+   lineNum = lineNum.slice( '--line='.length );
+
+   $('.purposeMain').html(
+      `File «<b>${msgFilePath}</b>», line <b>${lineNum}.</b>`
+   );
+});
